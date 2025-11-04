f972c0316a7e8f3856f409104fa983fc1bcc19cf
Mago <MagoDigi@users.noreply.huggingface.co>
2025-11-04 20:24:38 +0000

    Manual changes saved

--- components/navbar.js
+++ components/navbar.js
@@ -63,6 +63,7 @@ class CustomNavbar extends HTMLElement {
           transition: all 0.2s;
         }
         .cta-button:hover {
+          color: #ffffff;
           transform: translateY(-2px);
           box-shadow: 0 4px 12px rgba(255, 183, 0, 0.3);
         }
